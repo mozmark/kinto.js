a9e987e56e5cc7ec388ea3c3b433dd06a3fbb964
Mark Goodwin <mgoodwin@mozilla.com>
2015-09-29 21:42:46 +0100

    initial async impl

--- src/adapters/FirefoxStorage.js
+++ src/adapters/FirefoxStorage.js
@@ -2,25 +2,58 @@ Components.utils.import("resource://gre/modules/Services.jsm");
 Components.utils.import("resource://gre/modules/FileUtils.jsm");
 
 //class MyAdapter extends Kinto.adapters.BaseAdapter {
-class MyAdapter {
+export default class FirefoxAdapter {
   //constructor(dbname, dbconn) {
   constructor(dbname) {
     //super();
     this.dbname = dbname;
     //this.dbconn = dbconn;
 
-    let file = FileUtils.getFile("ProfD", ["kinto.sqlite"]);
-    this.dbconn = Services.storage.openDatabase(file);
+    this.file = FileUtils.getFile("ProfD", ["kinto.sqlite"]);
+    //this._dbconn = Services.storage.openDatabase(this.file);
 
     // attempt creation
-    this.dbconn.executeSimpleSQL("CREATE TABLE IF NOT EXISTS collection_metadata (collection_name TEXT PRIMARY KEY, last_modified INTEGER) WITHOUT ROWID");
-    this.dbconn.executeSimpleSQL("CREATE TABLE IF NOT EXISTS collection_data (collection_name TEXT, record_id TEXT, record TEXT);");
-    this.dbconn.executeSimpleSQL("CREATE UNIQUE INDEX IF NOT EXISTS unique_collection_record ON collection_data(collection_name, record_id); ");
+    var dbconn = this.getConnection();
+    var callback = this.connectionClosed.bind(this);
+    //TODO: split into multiple statements, retain single transaction
+    var statement = dbconn.createStatement(
+        "CREATE TABLE IF NOT EXISTS collection_metadata (collection_name TEXT PRIMARY KEY, last_modified INTEGER) WITHOUT ROWID; "
+        +"CREATE TABLE IF NOT EXISTS collection_data (collection_name TEXT, record_id TEXT, record TEXT); "
+        +"CREATE UNIQUE INDEX IF NOT EXISTS unique_collection_record ON collection_data(collection_name, record_id); ");
+
+    statement.executeAsync({
+      handleError: function(aError) {
+        dump("Error: " + aError.message);
+      },
+
+      handleCompletion: function(aReason) {
+        if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
+          dump("Query canceled or aborted!");
+        } else {
+          dump("Query complete");
+        }
+      }});
+    statement.finalize();
+    dbconn.asyncClose(callback);
   }
+
+getConnection() {
+  if (!this._dbconn) {
+    this._dbconn = Services.storage.openDatabase(this.file);
+  }
+  return this._dbconn;
+}
+
+connectionClosed() {
+  dump('connection closed\n');
+  this._dbconn = null;
+}
 
 clear() {
     // clear all of the data for this adapter
-    var statement = this.dbconn.createStatement("DELETE FROM collection_data WHERE collection_name = :collection_name;");
+    var dbconn = this.getConnection();
+    var callback = this.connectionClosed.bind(this);
+    var statement = dbconn.createStatement("DELETE FROM collection_data WHERE collection_name = :collection_name;");
     statement.params.collection_name = this.dbname;
 
     // execute the statement
@@ -39,12 +72,16 @@ clear() {
         }
       }
     });
+    statement.finalize();
+    dbconn.asyncClose(callback);
   }
 
   create(record) {
     if (record && record.id) {
       // insert a row for this record
-      var statement = this.dbconn.createStatement("INSERT INTO collection_data (collection_name, record_id, record) VALUES (:collection_name, :record_id, :record)");
+      var dbconn = this.getConnection();
+      var callback = this.connectionClosed.bind(this);
+      var statement = dbconn.createStatement("INSERT INTO collection_data (collection_name, record_id, record) VALUES (:collection_name, :record_id, :record)");
       statement.params.collection_name = this.dbname;
       statement.params.record_id = record.id;
       statement.params.record = JSON.stringify(record);
@@ -65,6 +102,8 @@ clear() {
           }
         }
       });
+      statement.finalize();
+      dbconn.asyncClose(callback);
     }
     // TODO: If we don't have a record or record ID, throw
   }
@@ -72,7 +111,9 @@ clear() {
   update(record) {
     // update the entry for this record
     if (record && record.id) {
-      var statement = this.dbconn.createStatement("UPDATE collection_data SET record = :record WHERE collection_name = :collection_name AND record_id = :record_id");
+      var dbconn = this.getConnection();
+      var callback = this.connectionClosed.bind(this);
+      var statement = dbconn.createStatement("UPDATE collection_data SET record = :record WHERE collection_name = :collection_name AND record_id = :record_id");
       statement.params.record = JSON.stringify(record);
       statement.params.collection_name = this.dbname;
       statement.params.record_id = record.id;
@@ -91,6 +132,7 @@ clear() {
           if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
             dump("Query canceled or aborted!");
           }
+          dbconn.asyncClose(callback);
         }
       });
     }
@@ -99,7 +141,9 @@ clear() {
   get(id) {
     // get a record with the specified ID
     if (id) {
-      var statement = this.dbconn.createStatement("SELECT record FROM collection_data WHERE collection_name = :collection_name AND record_id = :record_id");
+      var dbconn = this.getConnection();
+      var callback = this.connectionClosed.bind(this);
+      var statement = dbconn.createStatement("SELECT record FROM collection_data WHERE collection_name = :collection_name AND record_id = :record_id");
       statement.params.collection_name = this.dbname;
       statement.params.record_id = id;
 
@@ -126,6 +170,7 @@ clear() {
             if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
               dump("Query canceled or aborted!");
             }
+            dbconn.asyncClose(callback);
           }
         });
       });
@@ -135,7 +180,9 @@ clear() {
   delete(id) {
     if (id) {
       // delete the record with the specified ID
-      var statement = this.dbconn.createStatement("DELETE FROM collection_data WHERE collection_name = :collection_name AND record_id = :record_id");
+      var dbconn = this.getConnection();
+      var callback = this.connectionClosed.bind(this);
+      var statement = dbconn.createStatement("DELETE FROM collection_data WHERE collection_name = :collection_name AND record_id = :record_id");
       statement.params.collection_name = this.dbname;
       statement.params.record_id = id;
 
@@ -153,6 +200,7 @@ clear() {
           if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
             dump("Query canceled or aborted!");
           }
+          dbconn.asyncClose(callback);
         }
       });
     }
@@ -162,7 +210,9 @@ clear() {
 
   list() {
     // list the records
-    var statement = this.dbconn.createStatement("SELECT record FROM collection_data WHERE collection_name = :collection_name");
+    var dbconn = this.getConnection();
+    var callback = this.connectionClosed.bind(this);
+    var statement = dbconn.createStatement("SELECT record FROM collection_data WHERE collection_name = :collection_name");
     statement.params.collection_name = this.dbname;
 
     return new Promise(function(resolve, reject) {
@@ -188,6 +238,7 @@ clear() {
           if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
             dump("Query canceled or aborted!");
           }
+          dbconn.asyncClose(callback);
         }
       });
     });
@@ -197,7 +248,9 @@ clear() {
     // store the last modified data
     // TODO: ensure lastModified is a number?
     if (lastModified) {
-      var statement = this.dbconn.createStatement("REPLACE INTO collection_metadata (collection_name, last_modified) VALUES (:collection_name, :last_modified)");
+      var dbconn = this.getConnection();
+      var callback = this.connectionClosed.bind(this);
+      var statement = dbconn.createStatement("REPLACE INTO collection_metadata (collection_name, last_modified) VALUES (:collection_name, :last_modified)");
       statement.params.collection_name = this.dbname;
       statement.params.last_modified = lastModified;
 
@@ -215,6 +268,7 @@ clear() {
           if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
             dump("Query canceled or aborted!");
           }
+          dbconn.asyncClose(callback);
         }
       });
     }
@@ -224,7 +278,9 @@ clear() {
 
   getLastModified() {
     // retrieve the last modified data
-    var statement = this.dbconn.createStatement("SELECT last_modified FROM collection_metadata WHERE collection_name = :collection_name");
+    var dbconn = this.getConnection();
+    var callback = this.connectionClosed.bind(this);
+    var statement = dbconn.createStatement("SELECT last_modified FROM collection_metadata WHERE collection_name = :collection_name");
     statement.params.collection_name = this.dbname;
 
     return new Promise(function(resolve, reject) {
@@ -250,6 +306,7 @@ clear() {
           if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
             dump("Query canceled or aborted!");
           }
+          dbconn.asyncClose(callback);
         }
       });
     });
