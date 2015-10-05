2bdc24a4715e1cc655668f81dbbc7493494c73d5
Mark Goodwin <mgoodwin@mozilla.com>
2015-10-05 14:54:00 +0100

    resolved issue where list was failing on multiple result sets, renamed StorageInfo to KintoStorage

--- src/adapters/FirefoxStorage.js
+++ src/adapters/FirefoxStorage.js
@@ -6,16 +6,73 @@ function debug(message) {
   dump("FirefoxStorage "+message+"\n\n");
 }
 
+class KintoStorage {
+  constructor(dbname) {
+    this.dbname = dbname;
+    this.file = FileUtils.getFile("ProfD", ["kinto.sqlite"]);
+    this.statements = {};
+  }
+
+  getStatement(statementString) {
+    debug("getStatement for "+statementString);
+    let statement = this.statements[statementString];
+    if (statement) {
+      debug("statement exists");
+      if (statement.reset) {
+        statement.reset();
+      }
+    } else {
+      debug("statement does not exist; creating");
+      let dbconn = this.getConnection();
+      if (dbconn) {
+        statement = dbconn.createAsyncStatement(statementString);
+        this.statements[statementString] = statement;
+      } else {
+        debug("unable to get a database connection");
+      }
+    }
+    return statement;
+  }
+
+  getName() {
+    return this.dbname;
+  }
+
+  getConnection() {
+    if (!this._dbconn) {
+      this._dbconn = Services.storage.openDatabase(this.file);
+    }
+    return this._dbconn;
+  }
+
+  requestAsyncClose() {
+    // ensure all statements are finalized
+    for (let statementString in this.statements) {
+      debug("finalizing "+statementString);
+      this.statements[statementString].finalize();
+    }
+    // close the connection
+    if (this._dbconn) {
+      debug("closing the connection");
+      this._dbconn.asyncClose(this.connectionClosed.bind(this));
+    }
+  }
+
+  connectionClosed() {
+    debug('connection closed');
+    this._dbconn = null;
+    this.statements = {};
+  }
+}
+
 //class MyAdapter extends Kinto.adapters.BaseAdapter {
 export default class FirefoxAdapter {
   //constructor(dbname, dbconn) {
   constructor(dbname) {
     //super();
-    this.dbname = dbname;
     this.operations = [];
     this.busy = false;
 
-    this.file = FileUtils.getFile("ProfD", ["kinto.sqlite"]);
 
     // attempt creation
     // TODO: should these happen in a single transaction?
@@ -23,18 +80,74 @@ export default class FirefoxAdapter {
           "CREATE TABLE IF NOT EXISTS collection_data (collection_name TEXT, record_id TEXT, record TEXT);",
           "CREATE UNIQUE INDEX IF NOT EXISTS unique_collection_record ON collection_data(collection_name, record_id);"];
 
+    this.kintoStorage = new KintoStorage(dbname);
+
     for (var stmt of statements) {
       this.executeUpdate(stmt);
     }
   }
 
-executeUpdate(sql) {
-   debug("requesting to execute statement: "+sql);
-   this.executeOperation(function(dbconn, callback) {
+  executeUpdate(sql) {
+    debug("requesting to execute statement: "+sql);
+    this.executeOperation(function(kintoStorage, callback) {
       debug("executing statement: "+sql);
-      var statement = dbconn.createAsyncStatement(sql);
+      var statement = kintoStorage.getStatement(sql);
 
       statement.executeAsync({
+        handleError: function(aError) {
+          debug("Error: " + aError.message);
+        },
+
+          handleCompletion: function(aReason) {
+            if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
+              debug("Query canceled or aborted!");
+            } else {
+              debug("Query complete");
+            }
+            callback();
+          }
+      });
+    });
+  }
+
+  executeOperation(op) {
+    debug("executeOperation");
+    if (!this.busy) {
+      debug("Executing operation now");
+      this.busy = true;
+      function executeNextOperation() {
+        debug("executeNextOperation");
+        var next = this.operations.shift();
+        if (next) {
+          debug("executing queued operation");
+          next(this.kintoStorage, executeNextOperation.bind(this));
+        } else {
+          debug("cleaning up");
+          this.busy = false;
+          this.kintoStorage.requestAsyncClose();
+          debug("work queue complete");
+        }
+      }
+      op(this.kintoStorage, executeNextOperation.bind(this));
+    } else {
+      debug("queuing operation");
+      this.operations.push(op);
+    }
+  }
+
+  clear() {
+    this.executeOperation(function(kintoStorage, callback) {
+      debug("kinto::clear");
+      // clear all of the data for this adapter
+      var statement = kintoStorage.getStatement("DELETE FROM collection_data WHERE collection_name = :collection_name;");
+      statement.params.collection_name = kintoStorage.getName();
+
+      // execute the statement
+      statement.executeAsync({
+        handleResult: function(aResultSet) {
+          debug("A result set was not expected");
+        },
+
         handleError: function(aError) {
           debug("Error: " + aError.message);
         },
@@ -42,91 +155,20 @@ executeUpdate(sql) {
         handleCompletion: function(aReason) {
           if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
             debug("Query canceled or aborted!");
-          } else {
-            debug("Query complete");
           }
-          statement.finalize();
           callback();
         }
       });
     }.bind(this));
-}
-
-executeOperation(op) {
-  debug("executeOperation");
-  if (!this.busy) {
-    debug("Executing operation now");
-    var dbconn = this.getConnection();
-    var callback = this.connectionClosed.bind(this);
-    this.busy = true;
-    function executeNextOperation() {
-      debug("executeNextOperation");
-      var next = this.operations.shift();
-      if (next) {
-        debug("executing queued operation");
-        next(dbconn, executeNextOperation.bind(this));
-      } else {
-        debug("cleaning up");
-        this.busy = false;
-        dbconn.asyncClose(callback);
-        debug("work queue complete");
-      }
-    }
-    op(dbconn, executeNextOperation.bind(this));
-  } else {
-    debug("queuing operation");
-    this.operations.push(op);
   }
-}
-
-getConnection() {
-  if (!this._dbconn) {
-    this._dbconn = Services.storage.openDatabase(this.file);
-  }
-  return this._dbconn;
-}
-
-connectionClosed() {
-  debug('connection closed');
-  this._dbconn = null;
-}
-
-clear() {
-  this.executeOperation(function(dbconn, callback) {
-    debug("kinto::clear");
-    // clear all of the data for this adapter
-    var dbconn = this.getConnection();
-    var statement = dbconn.createAsyncStatement("DELETE FROM collection_data WHERE collection_name = :collection_name;");
-    statement.params.collection_name = this.dbname;
-
-    // execute the statement
-    statement.executeAsync({
-      handleResult: function(aResultSet) {
-        debug("A result set was not expected");
-      },
-
-      handleError: function(aError) {
-        debug("Error: " + aError.message);
-      },
-
-      handleCompletion: function(aReason) {
-        if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
-          debug("Query canceled or aborted!");
-        }
-        statement.finalize();
-        callback();
-      }
-    });
-  }.bind(this));
-}
 
   create(record) {
     if (record && record.id) {
-        this.executeOperation(function(dbconn, callback) {
+      this.executeOperation(function(kintoStorage, callback) {
         debug("kinto::create");
         // insert a row for this record
-        var statement = dbconn.createAsyncStatement("INSERT INTO collection_data (collection_name, record_id, record) VALUES (:collection_name, :record_id, :record)");
-        statement.params.collection_name = this.dbname;
+        var statement = kintoStorage.getStatement("INSERT INTO collection_data (collection_name, record_id, record) VALUES (:collection_name, :record_id, :record)");
+        statement.params.collection_name = kintoStorage.getName();
         statement.params.record_id = record.id;
         statement.params.record = JSON.stringify(record);
 
@@ -144,7 +186,6 @@ clear() {
             if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
               debug("Query canceled or aborted!");
             }
-            statement.finalize();
             callback();
           }
         });
@@ -156,11 +197,11 @@ clear() {
   update(record) {
     // update the entry for this record
     if (record && record.id) {
-      this.executeOperation(function(dbconn, callback) {
+      this.executeOperation(function(kintoStorage, callback) {
         debug("kinto::update");
-        var statement = dbconn.createAsyncStatement("UPDATE collection_data SET record = :record WHERE collection_name = :collection_name AND record_id = :record_id");
+        var statement = kintoStorage.getStatement("UPDATE collection_data SET record = :record WHERE collection_name = :collection_name AND record_id = :record_id");
         statement.params.record = JSON.stringify(record);
-        statement.params.collection_name = this.dbname;
+        statement.params.collection_name = kintoStorage.getName();
         statement.params.record_id = record.id;
 
         // execute the statement
@@ -177,7 +218,6 @@ clear() {
             if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
               debug("Query canceled or aborted!");
             }
-            statement.finalize();
             callback();
           }
         });
@@ -191,9 +231,9 @@ clear() {
       debug("kinto::get");
 
       return new Promise(function(resolve, reject) {
-        this.executeOperation(function(dbconn, callback) {
-          var statement = dbconn.createAsyncStatement("SELECT record FROM collection_data WHERE collection_name = :collection_name AND record_id = :record_id");
-          statement.params.collection_name = this.dbname;
+        this.executeOperation(function(kintoStorage, callback) {
+          var statement = kintoStorage.getStatement("SELECT record FROM collection_data WHERE collection_name = :collection_name AND record_id = :record_id");
+          statement.params.collection_name = kintoStorage.getName();
           statement.params.record_id = id;
 
           // execute the statement
@@ -201,11 +241,11 @@ clear() {
             handleResult: function(aResultSet) {
               debug("result set obtained:");
               for (let row = aResultSet.getNextRow(); row; row = aResultSet.getNextRow()) {
-                    let value = row.getResultByName("record");
-                    debug(value);
-                    var result = JSON.parse(value);
-                    return resolve(result);
-                  }
+                let value = row.getResultByName("record");
+                debug(value);
+                var result = JSON.parse(value);
+                return resolve(result);
+              }
               reject("oh noes!");
             },
 
@@ -218,7 +258,6 @@ clear() {
               if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
                 debug("Query canceled or aborted!");
               }
-              statement.finalize();
               callback();
             }
           });
@@ -229,12 +268,12 @@ clear() {
 
   delete(id) {
     if (id) {
-      this.executeOperation(function(dbconn, callback) {
+      this.executeOperation(function(kintoStorage, callback) {
         debug("kinto::delete");
         // delete the record with the specified ID
-        var dbconn = this.getConnection();
-        var statement = dbconn.createAsyncStatement("DELETE FROM collection_data WHERE collection_name = :collection_name AND record_id = :record_id");
-        statement.params.collection_name = this.dbname;
+        var kintoStorage = this.getConnection();
+        var statement = kintoStorage.getStatement("DELETE FROM collection_data WHERE collection_name = :collection_name AND record_id = :record_id");
+        statement.params.collection_name = kintoStorage.getName();
         statement.params.record_id = id;
 
         // execute the statement
@@ -251,7 +290,6 @@ clear() {
             if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
               debug("Query canceled or aborted!");
             }
-            statement.finalize();
             callback();
           }
         });
@@ -263,24 +301,24 @@ clear() {
 
   list() {
     return new Promise(function(resolve, reject) {
-      this.executeOperation(function(dbconn, callback){
+      this.executeOperation(function(kintoStorage, callback){
         debug("kinto::list");
 
         // list the records
-        var statement = dbconn.createAsyncStatement("SELECT record FROM collection_data WHERE collection_name = :collection_name");
-        statement.params.collection_name = this.dbname;
+        var statement = kintoStorage.getStatement("SELECT record FROM collection_data WHERE collection_name = :collection_name");
+        statement.params.collection_name = kintoStorage.getName();
+
+        var results = [];
 
         // execute the statement
         statement.executeAsync({
           handleResult: function(aResultSet) {
             debug("result set obtained:");
-            var results = [];
             for (let row = aResultSet.getNextRow(); row; row = aResultSet.getNextRow()) {
-                  let value = row.getResultByName("record");
-                  debug(value);
-                  results[results.length] = JSON.parse(value);
-                }
-            resolve(results);
+              let value = row.getResultByName("record");
+              debug(value);
+              results[results.length] = JSON.parse(value);
+            }
           },
 
           handleError: function(aError) {
@@ -291,8 +329,9 @@ clear() {
           handleCompletion: function(aReason) {
             if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
               debug("Query canceled or aborted!");
+            } else {
+              resolve(results);
             }
-            statement.finalize();
             callback();
           }
         });
@@ -304,9 +343,9 @@ clear() {
     // store the last modified data
     // TODO: ensure lastModified is a number?
     if (lastModified) {
-      this.executeOperation(function(dbconn, callback) {
-        var statement = dbconn.createAsyncStatement("REPLACE INTO collection_metadata (collection_name, last_modified) VALUES (:collection_name, :last_modified)");
-        statement.params.collection_name = this.dbname;
+      this.executeOperation(function(kintoStorage, callback) {
+        var statement = kintoStorage.getStatement("REPLACE INTO collection_metadata (collection_name, last_modified) VALUES (:collection_name, :last_modified)");
+        statement.params.collection_name = kintoStorage.getName();
         statement.params.last_modified = lastModified;
 
         // execute the statement
@@ -323,7 +362,6 @@ clear() {
             if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
               debug("Query canceled or aborted!");
             }
-            statement.finalize();
             callback();
           }
         });
@@ -335,10 +373,10 @@ clear() {
 
   getLastModified() {
     return new Promise(function(resolve, reject) {
-      this.executeOperation(function (dbconn, callback){
+      this.executeOperation(function (kintoStorage, callback){
         // retrieve the last modified data
-        var statement = dbconn.createAsyncStatement("SELECT last_modified FROM collection_metadata WHERE collection_name = :collection_name");
-        statement.params.collection_name = this.dbname;
+        var statement = kintoStorage.getStatement("SELECT last_modified FROM collection_metadata WHERE collection_name = :collection_name");
+        statement.params.collection_name = kintoStorage.getName();
 
         // execute the statement
         statement.executeAsync({
@@ -362,7 +400,6 @@ clear() {
             if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
               debug("Query canceled or aborted!");
             }
-            statement.finalize();
             callback();
           }
         });
